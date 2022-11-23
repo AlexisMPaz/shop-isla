207442c03c7b938f274703f5c0f895318df1266c
AlexisMPaz <alexismpaz@gmail.com>
2022-11-22 21:20:36 -0300

    Arreglo de clases

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,7 +2,7 @@ import "./CartWidget.css"
 const CartWidget = () => {
     return (
         <div className="d-flex">
-            <button className="btn cart"><i class="fa-solid fa-cart-shopping"></i></button>
+            <button className="btn cart"><i className="fa-solid fa-cart-shopping"></i></button>
             <p>0</p>
         </div>
     );
